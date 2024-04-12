a27ecff8655719c22faec0ffd13331258ee5d667
deathfluffy <mrandreu454@gmail.com>
2024-04-12 13:09:25 +0300

    add signIn form

--- src/components/LoginFrom/LoginFrom.jsx
+++ src/components/LoginFrom/LoginFrom.jsx
@@ -3,15 +3,18 @@ import { FaSignInAlt } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { ContentLoader } from 'components';
-
 import { SIGN_UP_ROUTE } from 'routes/routes';
 import { logInSelector } from 'store/selectors';
 import { notifyApi } from 'notify';
 import { logIn } from 'store/operations';
-
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import css from './LoginFrom.module.css';
+import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { useState } from 'react';
+import { VisibilityOffOutlined } from '@mui/icons-material';
 
 export const LoginFrom = () => {
+  const [visible, setVisible] = useState(false);
   const dispatch = useDispatch();
   const { isLoading } = useSelector(logInSelector);
 
@@ -32,29 +35,75 @@ export const LoginFrom = () => {
       true
     );
   };
-
+  const EndAdorment = ({ visible, setVisible }) => {
+    return (
+      <InputAdornment position="end">
+        <IconButton onClick={() => setVisible(!visible)}>
+          {visible ? <VisibilityOffOutlined /> : <RemoveRedEyeOutlinedIcon />}
+        </IconButton>
+      </InputAdornment>
+    );
+  };
   return (
     <div className={css.loginContainer}>
-      <h1 className={css.title}>Log In</h1>
+      <h1 className={css.title}>Sign In</h1>
       <form className={css.form} onSubmit={handleSubmit}>
         <label className={css.label}>
-          <span>Email:</span>
-          <input type="email" name="email" required />
+          <span>Enter your email</span>
+          <TextField
+            fullWidth
+            type="password"
+            name="password"
+            required
+            placeholder="Email"
+          />
         </label>
         <label className={css.label}>
-          <span>Password:</span>
-          <input type="password" name="password" required />
+          <span>Enter your password</span>
+          <TextField
+            fullWidth
+            style={{}}
+            type={visible ? 'text' : 'password'}
+            name="password"
+            required
+            placeholder="Password"
+            InputProps={{
+              endAdornment: (
+                <EndAdorment visible={visible} setVisible={setVisible} />
+              ),
+            }}
+          />
         </label>
         <button type="submit" className={css.button} disabled={isLoading}>
-          Log In &nbsp; {isLoading ? <ContentLoader /> : <FaSignInAlt />}
+          Sign In &nbsp; {isLoading ? <ContentLoader /> : <FaSignInAlt />}
         </button>
       </form>
       <div className={css.redirectLink}>
-        <p>Don't have an account yet?</p>
         <NavLink to={SIGN_UP_ROUTE} className={css.link}>
-          Register Now
+          Sign Up
         </NavLink>
       </div>
+      {/* <picture className="bottle">
+        <source
+          media="(min-width: 1440px)"
+          width="865"
+          height="680"
+          type="image/png"
+        />
+        <source
+          media="(min-width: 768px)"
+          width="656"
+          height="548"
+          type="image/png"
+        />
+        <source
+          media="(min-width: 320px)"
+          width="280"
+          height="210"
+          type="image/png"
+        />
+        <img className="bottle" alt="bottle of water" />
+      </picture> */}
     </div>
   );
 };
