80b6dc6f6e9d0764c8b9d9ba8a8615cc2f48f1dd
deathfluffy <mrandreu454@gmail.com>
2024-04-14 20:02:13 +0300

    fix Schema

--- src/schemasValdiate/LoginSchema.jsx
+++ src/schemasValdiate/LoginSchema.jsx
@@ -1,10 +1,6 @@
 import * as Yup from 'yup';
 
-const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
-
 export const LoginSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters long')
-    .matches(passwordRules, { message: 'Please enter a valid password' }),
+  password: Yup.string().min(8, 'Password must be at least 8 characters long'),
 });
