b24b9a9ca1a975c61957c1b17c58c845bc514c45
deathfluffy <mrandreu454@gmail.com>
2024-04-14 22:42:29 +0300

    fix Schema

--- src/schemasValdiate/LoginSchema.jsx
+++ src/schemasValdiate/LoginSchema.jsx
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 
 export const LoginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters long'),
+  email: Yup.string().email('Invalid email').required('Password is required'),
+  password: Yup.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .required('Password is required'),
 });
