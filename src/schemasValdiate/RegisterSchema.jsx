f16635cb26c43d13bf4729a9929a65bb1151542e
deathfluffy <mrandreu454@gmail.com>
2024-04-14 20:01:38 +0300

    fix Schema

--- src/schemasValdiate/RegisterSchema.jsx
+++ src/schemasValdiate/RegisterSchema.jsx
@@ -1,14 +1,6 @@
 import * as Yup from 'yup';
 
-const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
-
 export const RegisterForm = Yup.object().shape({
   email: Yup.string().email('Invalid email'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters long')
-    .matches(passwordRules, { message: 'Please create a stronger password' }),
-  passwordRepeat: Yup.string().oneOf(
-    [Yup.ref('password'), null],
-    'Passwords must match'
-  ),
+  password: Yup.string().min(8, 'Password must be at least 8 characters long'),
 });
