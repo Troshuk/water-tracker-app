8fe03019ee96258094a82ec9c5818fbe26f14503
deathfluffy <mrandreu454@gmail.com>
2024-04-20 18:22:34 +0300

    fix

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -2,125 +2,121 @@ import React, { useState } from 'react';
 import css from './MonthStatsTable.module.css';
 import useModal from 'components/customHooks/useModal';
 
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
 export const MonthStatsTable = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [selectedMonth, setSelectedMonth] = useState(null);
+  const [state, setState] = useState({
+    currentDate: new Date(),
+    selectedDate: null,
+    selectedMonth: null,
+  });
+
+  const { currentDate, selectedDate, selectedMonth } = state;
   const { isOpen, openModal, closeModal } = useModal();
 
-  const getDaysInMonth = date => {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    return new Date(year, month, 0).getDate();
-  };
+  const getDaysInMonth = date =>
+    new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
   const getMonthName = date => {
     const options = { month: 'long' };
     return new Intl.DateTimeFormat('en-US', options).format(date);
   };
 
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const goToPreviousMonth = () => {
-    setCurrentDate(
-      new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
-    );
-  };
-
-  const goToNextMonth = () => {
-    setCurrentDate(
-      new Date(currentDate.getFullYear(), currentDate.getMonth() + 1)
-    );
+  const goToMonth = increment => {
+    setState(prevState => ({
+      ...prevState,
+      currentDate: new Date(
+        prevState.currentDate.getFullYear(),
+        prevState.currentDate.getMonth() + increment
+      ),
+    }));
   };
 
   const handleDateClick = day => {
-    setSelectedDate(day);
-    setSelectedMonth(currentDate.getMonth());
+    setState(prevState => ({
+      ...prevState,
+      selectedDate: day,
+      selectedMonth: prevState.currentDate.getMonth(),
+    }));
     openModal();
   };
-  const renderCalendar = () => {
-    const daysInMonth = getDaysInMonth(currentDate);
-    const monthName = getMonthName(currentDate);
 
-    const days = [];
+  const daysInMonth = getDaysInMonth(currentDate);
+  const monthName = getMonthName(currentDate);
 
-    Array.from({ length: daysInMonth }).forEach((_, i) => {
-      const day = i + 1;
-      days.push(
-        <div className={css.containerList} key={`day-${day}`}>
-          <button
-            className={css.buttonCalendar}
-            onClick={() => handleDateClick(day)}
-          >
-            <li className={css.calendarDay}>{day}</li>
-          </button>
-          <p className={css.itemCalendary}>100%</p>
-        </div>
-      );
-    });
+  const days = [];
 
-    return (
-      <div className={css.calendar}>
-        <div className={css.calendarHeader}>
-          <h1 className={css.title}>Month</h1>
-          <div className={css.containerCalendar}>
-            <button onClick={goToPreviousMonth} className={css.itemButton}>
-              &lt;
-            </button>
-            <h2 className={css.itemName}>
-              {monthName},{currentDate.getFullYear()}
-            </h2>
-            <button onClick={goToNextMonth} className={css.itemButton}>
-              &gt;
-            </button>
-          </div>
-        </div>
-        <div className={css.calendarBody}>
-          {isOpen && (
-            <div className={css.modalBackground} onClick={closeModal}>
-              <div
-                className={css.modalContent}
-                onClick={e => e.stopPropagation()}
-              >
-                <p>
-                  <span className={css.selectedTimes}>
-                    {selectedDate}, {monthNames[selectedMonth]}
-                  </span>
-                </p>
-                <p className={css.titleModal}>
-                  Daily norma:
-                  <span className={css.selectedTimes}> 1.8L</span>
-                </p>
-                <p className={css.titleModal}>
-                  Fulfillment of the daily norm:
-                  <span className={css.selectedTimes}> 100%</span>
-                </p>
-                <p className={css.titleModal}>
-                  How many servings of water:
-                  <span className={css.selectedTimes}> 6</span>
-                </p>
-                <button onClick={closeModal}>Close Modal</button>
-              </div>
-            </div>
-          )}
-          <ul className={css.calendarList}>{days}</ul>
-        </div>
+  Array.from({ length: daysInMonth }).forEach((_, i) => {
+    const day = i + 1;
+    days.push(
+      <div className={css.containerList} key={`day-${day}`}>
+        <button
+          className={css.buttonCalendar}
+          onClick={() => handleDateClick(day)}
+        >
+          <li className={css.calendarDay}>{day}</li>
+        </button>
+        <p className={css.itemCalendary}>100%</p>
       </div>
     );
-  };
+  });
 
-  return renderCalendar();
+  return (
+    <div className={css.calendar}>
+      <div className={css.calendarHeader}>
+        <h1 className={css.title}>Month</h1>
+        <div className={css.containerCalendar}>
+          <button onClick={() => goToMonth(-1)} className={css.itemButton}>
+            &lt;
+          </button>
+          <h2 className={css.itemName}>
+            {monthName},{currentDate.getFullYear()}
+          </h2>
+          <button onClick={() => goToMonth(1)} className={css.itemButton}>
+            &gt;
+          </button>
+        </div>
+      </div>
+      <div className={css.calendarBody}>
+        {isOpen && (
+          <div className={css.modalBackground}>
+            <div className={css.modalContent}>
+              <p>
+                <span className={css.selectedTimes}>
+                  {selectedDate}, {monthNames[selectedMonth]}
+                </span>
+              </p>
+              <p className={css.titleModal}>
+                Daily norma:
+                <span className={css.selectedTimes}> 1.8L</span>
+              </p>
+              <p className={css.titleModal}>
+                Fulfillment of the daily norm:
+                <span className={css.selectedTimes}> 100%</span>
+              </p>
+              <p className={css.titleModal}>
+                How many servings of water:
+                <span className={css.selectedTimes}> 6</span>
+              </p>
+              <button onClick={closeModal}>Close Modal</button>
+            </div>
+          </div>
+        )}
+        <ul className={css.calendarList}>{days}</ul>
+      </div>
+    </div>
+  );
 };
