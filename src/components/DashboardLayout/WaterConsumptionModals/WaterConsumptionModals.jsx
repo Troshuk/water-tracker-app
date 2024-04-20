6aac898e8a7aec1c8ad088157ea141c621373ef1
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2024-04-20 19:13:37 +0300

    finished AddWater Modal

--- src/components/DashboardLayout/WaterConsumptionModals/WaterConsumptionModals.jsx
+++ src/components/DashboardLayout/WaterConsumptionModals/WaterConsumptionModals.jsx
@@ -1,5 +1,5 @@
 import ReactModal from 'react-modal';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Icon } from 'components';
 import css from './WaterConsumptionModals.module.css';
 import { useDispatch } from 'react-redux';
@@ -9,10 +9,19 @@ import { createConsumptionRecord } from '../../../store/water/operations.js';
 ReactModal.setAppElement('#root');
 
 const getCurrentTime = () => {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  return `${hours}:${minutes}`;
+  const today = new Date();
+  const roundedMinutes = Math.round(today.getMinutes() / 5.0) * 5;
+  const formattedMinutes = String(roundedMinutes).padStart(2, '0');
+
+  const closes = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    today.getHours(),
+    formattedMinutes
+  );
+  // console.log('closes', closes.toISOString());
+  return closes.toISOString();
 };
 
 // Генеруємо варіанти часу з інтервалом 5 хвилин
@@ -21,19 +30,20 @@ const generateTimeOptions = () => {
   const today = new Date();
   for (let hour = 0; hour < 24; hour++) {
     for (let minute = 0; minute < 60; minute += 5) {
-      const DateOption = new Date(
+      const time = `${String(hour).padStart(1, '0')}:${String(minute).padStart(
+        2,
+        '0'
+      )}`;
+      const dateTime = new Date(
         today.getFullYear(),
         today.getMonth(),
         today.getDate(),
         hour,
         minute
-      ).toISOString();
-      const time = `${String(hour).padStart(1, '0')}:${String(minute).padStart(
-        2,
-        '0'
-      )}`;
+      );
+      const dateOption = dateTime.toISOString();
       options.push(
-        <option key={DateOption} value={DateOption}>
+        <option key={dateOption} value={dateOption}>
           {time}
         </option>
       );
@@ -42,30 +52,55 @@ const generateTimeOptions = () => {
   return options;
 };
 
+const currentTime = getCurrentTime();
+
 export const WaterConsumptionAddModal = ({ isOpen, onRequestClose }) => {
-  const [selectedTime, setSelectedTime] = useState(getCurrentTime());
-  const [consumedValue, setConsumedValue] = useState(0);
-  const [changedConsumedValue, setchangedConsumedValue] = useState('0');
+  const [selectedTime, setSelectedTime] = useState(currentTime);
+  const [consumedValue, setConsumedValue] = useState(Number(50));
+  const [changedConsumedValue, setChangedConsumedValue] = useState(Number(50));
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    setSelectedTime(getCurrentTime());
+  }, []);
 
   const handleTimeChange = event => {
     setSelectedTime(event.target.value);
   };
+  const changeConsumedValue = e => {
+    console.log(e);
+    const num = Number(e.target.value);
+    num >= 0 && num <= 5000
+      ? setChangedConsumedValue(num)
+      : setConsumedValue(changedConsumedValue);
+  };
+
   const handleValueChange = e => {
     setConsumedValue(e.target.value);
   };
-  const changeConsumedValue = () => {
-    setchangedConsumedValue(consumedValue);
+
+  const plus50ml = () => {
+    const value = Number(consumedValue);
+    setConsumedValue(value + 50 <= 5000 ? value + 50 : value);
+    setChangedConsumedValue(value + 50 <= 5000 ? value + 50 : value);
   };
+
+  const minus50ml = () => {
+    const value = Number(consumedValue);
+    setConsumedValue(value - 50 >= 0 ? value - 50 : value);
+    setChangedConsumedValue(value - 50 >= 0 ? value - 50 : value);
+  };
+
   const handleSubmit = () => {
     dispatch(
       createConsumptionRecord({
         value: changedConsumedValue,
-        consumed_at: selectedTime,
+        consumed_at: new Date(selectedTime).toISOString(),
       })
     );
   };
+
   return (
     <ReactModal
       isOpen={isOpen}
@@ -93,7 +128,7 @@ export const WaterConsumptionAddModal = ({ isOpen, onRequestClose }) => {
         <p className={css.amount}>Amount of water:</p>
         <div>
           <div className={css.addPanel}>
-            <button className={css.plus_minus}>
+            <button className={css.plus_minus} onClick={minus50ml}>
               <Icon
                 className={css.little_plus_minus}
                 id="minus"
@@ -102,7 +137,7 @@ export const WaterConsumptionAddModal = ({ isOpen, onRequestClose }) => {
               />
             </button>
             <span className={css.amount_to_add}>{changedConsumedValue}ml</span>
-            <button className={css.plus_minus}>
+            <button className={css.plus_minus} onClick={plus50ml}>
               <Icon
                 className={css.little_plus_minus}
                 id="plus"
@@ -120,7 +155,7 @@ export const WaterConsumptionAddModal = ({ isOpen, onRequestClose }) => {
               onChange={handleTimeChange}
               className={css.select_time_value}
             >
-              {generateTimeOptions()}
+              {selectedTime},{generateTimeOptions()}
             </select>
           </div>
           <div className={css.container_select}>
@@ -128,12 +163,13 @@ export const WaterConsumptionAddModal = ({ isOpen, onRequestClose }) => {
               Enter the value of the water used:
             </label>
             <input
-              type="input"
+              type="number"
               className={css.select_time_value}
               onChange={handleValueChange}
               onBlur={changeConsumedValue}
               value={consumedValue}
-            ></input>
+              min={0}
+            />
           </div>
           <div className={css.container_save}>
             <span className={css.value_save}>{changedConsumedValue}ml</span>
