baef27670a572ec91b9a7f589a1f9cab060e5fc5
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-04-22 00:39:04 +0300

    upd

--- src/components/Verify/Verify.jsx
+++ src/components/Verify/Verify.jsx
@@ -1,23 +1,25 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { notifyApi } from 'notify';
 
-import { Container, Section } from 'components';
+import { Container, Section, ContentLoader } from 'components';
 
 import css from './Verify.module.css';
 
 import { useEffect } from 'react';
 import { verifyEmail } from 'store/operations';
 import { LOGIN_ROUTE } from 'routes/routes';
+import { verifyEmailSelector } from 'store/selectors';
 
-export const Verify = ({ email }) => {
+export const Verify = () => {
+  const { isLoading } = useSelector(verifyEmailSelector);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { token } = useParams();
+  const {token } = useParams();
 
   useEffect(() => {
     notifyApi(
-      dispatch(verifyEmail({ token }))
+      dispatch(verifyEmail( {token} ))
         .unwrap()
         .then(() => {
           navigate(LOGIN_ROUTE);
@@ -29,7 +31,9 @@ export const Verify = ({ email }) => {
 
   return (
     <Section className={css.sectionForm}>
-      <Container className={css.Container}></Container>
+      <Container className={css.Container}>
+      {isLoading && <ContentLoader />}
+      </Container>
     </Section>
   );
 };
