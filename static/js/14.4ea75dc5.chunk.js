8822a7740442e447bdad683f6f68cf24319298bb
Troshuk <Troshuk@gmail.com>
2024-04-21 05:01:02 +0000

    Deploying to gh-pages from @ Troshuk/water-tracker-app@cc639568fa7b7286b4891093ace94b6d50b32cdb 🚀

---
+++ static/js/14.4ea75dc5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwater_tracker_app=self.webpackChunkwater_tracker_app||[]).push([[14],{7014:(e,r,a)=>{a.r(r),a.d(r,{default:()=>u});var t=a(2792),p=a(579);function u(){return(0,p.jsx)(t.Yq,{})}}}]);
+//# sourceMappingURL=14.4ea75dc5.chunk.js.map
