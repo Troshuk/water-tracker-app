e22ad137ea8fe14933e9f37baf81bfa492e28ec6
deathfluffy <mrandreu454@gmail.com>
2024-04-21 17:55:01 +0300

    fix?

--- src/components/PageLoader/PageLoader.jsx
+++ src/components/PageLoader/PageLoader.jsx
@@ -4,21 +4,19 @@ import { Hourglass } from 'react-loader-spinner';
 export const PageLoader = props => {
   return (
     <Section>
-      <Container
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
+      <Container>
         <Hourglass
+          wrapperStyle={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
           visible={true}
-          height="80"
-          width="80"
+          height="150"
+          width="150"
           {...props}
           ariaLabel="hourglass-loading"
-          wrapperStyle={{}}
           wrapperClass=""
           colors={['#306cce', '#72a1ed']}
         />
