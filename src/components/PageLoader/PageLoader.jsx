3323ad9b013c5d7affa81be649479f75eda9ad2c
deathfluffy <mrandreu454@gmail.com>
2024-04-21 14:13:16 +0300

    replace loader

--- src/components/PageLoader/PageLoader.jsx
+++ src/components/PageLoader/PageLoader.jsx
@@ -1,26 +1,20 @@
 import { Container, Section } from 'components';
-import ContentLoader from 'react-content-loader';
+import { Hourglass } from 'react-loader-spinner';
 
 export const PageLoader = props => {
   return (
     <Section>
       <Container>
-        <ContentLoader
+        <Hourglass
           speed={1}
-          width="800"
-          height={300}
-          viewBox="0 0 400 160"
-          backgroundColor="#333"
-          foregroundColor="#555"
+          visible={true}
+          height={80}
+          width="80"
+          ariaLabel="hourglass-loading"
           {...props}
-        >
-          <rect x="20" y="8" rx="5" ry="5" width="60%" height="10" />
-          <rect x="20" y="26" rx="5" ry="5" width="40%" height="10" />
-          <rect x="0" y="56" rx="5" ry="5" width="100%" height="10" />
-          <rect x="0" y="72" rx="5" ry="5" width="90%" height="10" />
-          <rect x="0" y="88" rx="5" ry="5" width="60%" height="10" />
-          <circle cx="20" cy="120" r="15" />
-        </ContentLoader>
+          wrapperClass=""
+          colors={['#306cce', '#72a1ed']}
+        />
       </Container>
     </Section>
   );
